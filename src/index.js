b4bffd1781b6ef29de549f163cdf9efda108f0ec
Sultan Mohammad <mohaimin.bonny@gmail.com>
2020-11-09 01:48:41 +0600

    socket init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+server.listen(port, () => {
     console.log(`Server is up at port ${port}`);
 })
