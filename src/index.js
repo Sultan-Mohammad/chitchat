0d0565f84116ea690c6f2cd8d8d0b4d6d7494d69
Sultan Mohammad <mohaimin.bonny@gmail.com>
2020-11-08 14:23:38 +0600

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = 
+ d
+
+
+
+
+
+
+
+ `-+---`
+app.listen(port, () => {
+    console.log(`Server is up at ${port}`);
+});
